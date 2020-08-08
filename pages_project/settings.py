73c1504c27c913d360d50be30e2327c020c4e7cb
Yegua Celestial <yeguacelestial@gmail.com>
2020-08-08 17:13:22 -0500

    Modified settings

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'pages_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'), # Added templates folder created on Django project
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
