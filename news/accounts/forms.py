c0f59dfa9364d90648a635c34a6bddcf5de5e6b9
Yegua Celestial <yeguacelestial@gmail.com>
2020-10-15 17:54:59 -0500

    Initialize News app, and create custom user model

---
+++ news/accounts/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+# Add custom field to the default User model of Django
+class CustomUserCreationForm(UserChangeForm):
+
+    # Define model with new field, and add it to the UserCreatinForm
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age', )
+
+    # Do the same, but now for Custom User Change form
+    class CustomUserChangeForm(UserChangeForm):
+
+        class Meta():
+            model = CustomUser
+            fields = UserChangeForm.Meta.fields
